7f23141973020d9deea642fa54d56798cc11894b
NextWork123 <nextworks@protonmail.com>
2023-09-02 09:19:38 +0200

    Optimize Electron app code structure and handling of command-line arguments

--- src/main.ts
+++ src/main.ts
@@ -1,130 +1,130 @@
-// Modules to control application life and create native browser window
-import {BrowserWindow, app, crashReporter, session} from "electron";
+import { app, BrowserWindow, crashReporter, session } from "electron";
 import "v8-compile-cache";
+import path from "path";
+
 import {
-    Settings,
-    checkForDataFolder,
-    checkIfConfigExists,
-    firstRun,
-    getConfig,
-    getConfigSync,
-    injectElectronFlags,
-    installModLoader,
-    modInstallState,
-    setConfig,
-    setLang,
-    setWindowState,
-    sleep
+  Settings,
+  checkForDataFolder,
+  checkIfConfigExists,
+  firstRun,
+  getConfig,
+  getConfigSync,
+  injectElectronFlags,
+  installModLoader,
+  setConfig,
+  setLang,
+  setWindowState,
+  sleep
 } from "./utils";
+
 import "./extensions/mods";
 import "./tray";
-import {createCustomWindow, createNativeWindow, createTransparentWindow, mainWindow} from "./window";
-import path from "path";
-import {createTManagerWindow} from "./themeManager/main";
-import {createSplashWindow} from "./splash/main";
-import {createSetupWindow} from "./setup/main";
-import {createKeybindWindow} from "./keybindMaker/main";
+import { createCustomWindow, createNativeWindow, createTransparentWindow, mainWindow } from "./window";
+import { createTManagerWindow } from "./themeManager/main";
+import { createSplashWindow } from "./splash/main";
+import { createSetupWindow } from "./setup/main";
+import { createKeybindWindow } from "./keybindMaker/main";
+
 export let iconPath: string;
 export let settings: any;
 export let customTitlebar: boolean;
-app.on("render-process-gone", (event, webContents, details) => {
-    if (details.reason == "crashed") {
-        app.relaunch();
-    }
-});
-async function args(): Promise<void> {
-    let argNum = 2;
-    if (process.argv[0] == "electron") argNum++;
-    let args = process.argv[argNum];
-    if (args == undefined) return;
-    if (args.startsWith("--")) return; //electron flag
-    if (args.includes("=")) {
-        let e = args.split("=");
-        await setConfig(e[0] as keyof Settings, e[1]);
-        console.log(`Setting ${e[0]} to ${e[1]}`);
-        app.relaunch();
-        app.exit();
-    } else if (args == "themes") {
-        app.whenReady().then(async () => {
-            createTManagerWindow();
-        });
-    } else if (args == "keybinds") {
-        app.whenReady().then(async () => {
-            createKeybindWindow();
-        });
-    }
-}
-args(); // i want my top level awaits
-if (!app.requestSingleInstanceLock() && getConfigSync("multiInstance") == (false ?? undefined)) {
-    // if value isn't set after 3.2.4
-    // kill if 2nd instance
-    app.quit();
-} else {
-    app.commandLine.appendSwitch("disable-features", "WidgetLayering"); // fix dev tools layers
-    // Your data now belongs to CCP
-    crashReporter.start({uploadToServer: false});
-    // enable webrtc capturer for wayland
-    if (process.platform === "linux" && process.env.XDG_SESSION_TYPE?.toLowerCase() === "wayland") {
-        app.commandLine.appendSwitch("enable-features=WebRTCPipeWireCapturer");
-        console.log("Wayland detected, using PipeWire for video capture.");
-    }
-    // work around chrome 66 disabling autoplay by default
-    app.commandLine.appendSwitch("autoplay-policy", "no-user-gesture-required");
 
-    // WinRetrieveSuggestionsOnlyOnDemand: Work around electron 13 bug w/ async spellchecking on Windows.
-    // HardwareMediaKeyHandling,MediaSessionService: Prevent Discord from registering as a media service.
-    app.commandLine.appendSwitch(
-        "disable-features",
-        "WinRetrieveSuggestionsOnlyOnDemand,HardwareMediaKeyHandling,MediaSessionService"
-    );
-    checkForDataFolder();
-    checkIfConfigExists();
-    injectElectronFlags();
-    app.whenReady().then(async () => {
-        if ((await getConfig("customIcon")) !== undefined ?? null) {
-            iconPath = await getConfig("customIcon");
-        } else {
-            iconPath = path.join(__dirname, "../", "/assets/desktop.png");
-        }
-        async function init(): Promise<void> {
-            if ((await getConfig("skipSplash")) == false) {
-                createSplashWindow();
-            }
-            if (firstRun == true) {
-                await setLang(new Intl.DateTimeFormat().resolvedOptions().locale);
-                createSetupWindow();
-            }
-            switch (await getConfig("windowStyle")) {
-                case "default":
-                    createCustomWindow();
-                    customTitlebar = true;
-                    break;
-                case "native":
-                    createNativeWindow();
-                    break;
-                case "transparent":
-                    createTransparentWindow();
-                    break;
-                default:
-                    createCustomWindow();
-                    customTitlebar = true;
-                    break;
-            }
-        }
-        await init();
-        await installModLoader();
-        session.fromPartition("some-partition").setPermissionRequestHandler((_webContents, permission, callback) => {
-            if (permission === "notifications") {
-                // Approves the permissions request
-                callback(true);
-            }
-            if (permission === "media") {
-                // Approves the permissions request
-                callback(true);
-            }
-        });
-        app.on("activate", async function () {
-            if (BrowserWindow.getAllWindows().length === 0) await init();
-        });
+app.on("render-process-gone", (event, webContents, details) => {
+  if (details.reason === "crashed") {
+    app.relaunch();
+  }
+});
+
+if (!app.requestSingleInstanceLock() && getConfigSync("multiInstance") === false) {
+  app.quit();
+} else {
+  app.commandLine.appendSwitch("disable-features", "WidgetLayering");
+  crashReporter.start({ uploadToServer: false });
+
+  if (process.platform === "linux" && process.env.XDG_SESSION_TYPE?.toLowerCase() === "wayland") {
+    app.commandLine.appendSwitch("enable-features=WebRTCPipeWireCapturer");
+    console.log("Wayland detected, using PipeWire for video capture.");
+  }
+
+  app.commandLine.appendSwitch("autoplay-policy", "no-user-gesture-required");
+  app.commandLine.appendSwitch(
+    "disable-features",
+    "WinRetrieveSuggestionsOnlyOnDemand,HardwareMediaKeyHandling,MediaSessionService"
+  );
+
+  checkForDataFolder();
+  checkIfConfigExists();
+  injectElectronFlags();
+
+  app.whenReady().then(async () => {
+    if ((await getConfig("customIcon")) !== undefined || null) {
+      iconPath = await getConfig("customIcon");
+    } else {
+      iconPath = path.join(__dirname, "../", "/assets/desktop.png");
+    }
+
+    async function init(): Promise<void> {
+      if (!(await getConfig("skipSplash"))) {
+        createSplashWindow();
+      }
+      if (firstRun) {
+        await setLang(new Intl.DateTimeFormat().resolvedOptions().locale);
+        createSetupWindow();
+      }
+
+      switch (await getConfig("windowStyle")) {
+        case "native":
+          createNativeWindow();
+          break;
+        case "transparent":
+          createTransparentWindow();
+          break;
+        default:
+          createCustomWindow();
+          customTitlebar = true;
+          break;
+      }
+    }
+
+    await init();
+    await installModLoader();
+
+    session.fromPartition("some-partition").setPermissionRequestHandler((_webContents, permission, callback) => {
+      if (permission === "notifications" || permission === "media") {
+        callback(true);
+      }
     });
+
+    app.on("activate", async () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        await init();
+      }
+    });
+
+    // Handle command-line arguments
+    let argNum = 2;
+    if (process.argv[0] === "electron") {
+      argNum++;
+    }
+    const args = process.argv[argNum];
+    if (args && !args.startsWith("--")) {
+      switch (args) {
+        case "themes":
+          createTManagerWindow();
+          break;
+        case "keybinds":
+          createKeybindWindow();
+          break;
+        default:
+          if (args.includes("=")) {
+            const [key, value] = args.split("=");
+            await setConfig(key as keyof Settings, value);
+            console.log(`Setting ${key} to ${value}`);
+            app.relaunch();
+            app.exit();
+          }
+          break;
+      }
+    }
+  });
 }
